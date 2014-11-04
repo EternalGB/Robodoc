6232b27b00c46e4b49b380fa7e1ad95a7f8219a7
RaphByrne <raph.byrne@gmail.com>
2014-11-04 15:36:20 +0800

    Probability tables

---
+++ Assets/Editor/ProbabilityTableEditor.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(ProbabilityTable))]
+public class WeightedProbabilityEditor : Editor
+{
+
+	public override void OnInspectorGUI ()
+	{
+		ProbabilityTable wpt = (ProbabilityTable)target;
+
+		EditorGUILayout.BeginVertical();
+		EditorGUILayout.LabelField("Items");
+		float tmpProb = 0;
+		for(int i = 0; i < wpt.Size; i++) {
+			tmpProb = wpt.probabilities[i];
+			EditorGUILayout.BeginHorizontal();
+			wpt.items[i] = EditorGUILayout.ObjectField(wpt.items[i],typeof(Object),true,GUILayout.Width(200));
+			tmpProb = EditorGUILayout.Slider(tmpProb,0,1);//EditorGUILayout.FloatField(tmpProb,GUILayout.Width(100));
+			if(GUI.changed)
+				wpt.SetProbability(i,tmpProb);
+			if(GUILayout.Button("Remove"))
+				wpt.RemoveEntry(i);
+			EditorGUILayout.EndHorizontal();
+		}
+
+		if(GUILayout.Button("Add Entry")) {
+			wpt.AddItem(null);
+		}
+
+		if(GUILayout.Button ("Reset Probabilities")) {
+			wpt.ResetProbabilities();
+		}
+		EditorGUILayout.EndVertical();
+		EditorGUIUtility.LookLikeControls();
+
+	}
+
+}
+
---
+++ Assets/Scripts/ProbabilityTable/ProbabilityTable.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+[ExecuteInEditMode]
+public class ProbabilityTable : ScriptableObject
+{
+	
+	public List<Object> items;
+	public List<float> probabilities;
+
+	public int Size
+	{
+		get {return items.Count;}
+	}
+
+	float maxProb;
+
+	public ProbabilityTable()
+	{
+		items = new List<Object>();
+		probabilities = new List<float>();
+		maxProb = 0;
+	}
+
+	public void AddItem(Object newItem)
+	{
+		float n = items.Count;
+		if(n > 0) {
+			float smallestProb = GetSmallestProb(probabilities);
+			float newSmallestProb = (n/(n+1))*smallestProb;
+			for(int i = 0; i < probabilities.Count; i++) {
+				probabilities[i] = (probabilities[i]/smallestProb)*newSmallestProb;
+			}
+			items.Add(newItem);
+			probabilities.Add(newSmallestProb);
+			maxProb = GetMaxProb();
+		} else {
+			items.Add(newItem);
+			probabilities.Add(1);
+			maxProb = 1;
+		}
+	}
+
+	float GetMaxProb()
+	{
+		float best = float.MaxValue;
+		foreach(float p in probabilities) {
+			if(p > best)
+				best = p;
+		}
+		return best;
+	}
+
+	public void SetProbability(int i, float newProb)
+	{
+		float difference = (probabilities[i] - newProb);
+		if(difference < 0) {
+			int numZeroes = 0;
+			for(int j = 0; j < Size; j++) {
+				if(probabilities[j] <= 0)
+					numZeroes++;
+			}
+			difference /= Size-numZeroes-1;
+			for(int j = 0; j < Size; j++)
+				if(i != j && probabilities[j]+difference > 0)
+					probabilities[j] += difference;
+
+		} else {
+			int numOnes = 0;
+			for(int j = 0; j < Size; j++) {
+				if(probabilities[j] == 0)
+					numOnes++;
+			}
+			difference /= Size-numOnes-1;
+			for(int j = 0; j < Size; j++)
+				if(i != j && probabilities[j]+difference < 1)
+					probabilities[j] += difference;
+		}
+		probabilities[i] = newProb;
+		maxProb = GetMaxProb();
+	}
+
+	public void RemoveEntry(int i)
+	{
+		float prob = probabilities[i]/(Size-1);
+		for(int j = 0; j < Size; j++)
+			if(i != j)
+				probabilities[j] += prob;
+		probabilities.RemoveAt(i);
+		items.RemoveAt (i);
+	}
+
+	static float GetSmallestProb(List<float> probs)
+	{
+		float best = float.MaxValue;
+		foreach(float p in probs) {
+			if(p < best)
+				best = p;
+		}
+		return best;
+	}
+
+
+	public Object GetRandom()
+	{
+		float rand;
+		int k = 0;
+		bool notAccepted = true;
+		while(notAccepted) {
+			rand = Random.value;
+			k = (int)(rand*items.Count)+1;
+			rand = Random.value;
+			if(rand < probabilities[k]/maxProb)
+				notAccepted = false;
+		}
+		return items[k];
+	}
+
+	public void ResetProbabilities()
+	{
+		float prob = 1f/Size;
+		for(int i = 0; i < Size; i++) {
+			probabilities[i] = prob;
+		}
+	}
+
+
+
+}
+
