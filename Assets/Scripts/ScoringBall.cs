664c388873fb24f776f68db211bda77f495b2634
RaphByrne <raph.byrne@gmail.com>
2015-01-13 12:21:43 +0800

    fixed problem where particle colour for bonus balls was coming out red

--- Assets/Scripts/ScoringBall.cs
+++ Assets/Scripts/ScoringBall.cs
@@ -42,11 +42,13 @@ public class ScoringBall : PoolableObject
 
 	public static Color GetParticleColor(Sprite sprite)
 	{
+
 		Color color = Util.GetAverageColor(sprite.texture,2);
+		//Debug.Log ("Sprite " + sprite.name + " average colour: " + color.ToString());
 		HSBColor hColor = new HSBColor(color);
-		hColor.s = 1;
+		//hColor.s = 1;
 		hColor.b = 1;
-		//Debug.Log ("Sample color " + hColor.ToString());
+		//Debug.Log ("Sprite " + sprite.name + " hcolour: " + hColor.ToString());
 		return hColor.ToColor();
 	}
 
