5fee526372ca3f9a78338a86fa9d613fa8e35229
RaphByrne <raph.byrne@gmail.com>
2015-03-11 16:18:23 +0800

    added random play space level

---
+++ Assets/Scripts/ChallengeMode/PlaySpaceScoreAreaLevelConstructor.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlaySpaceScoreAreaLevelConstructor : MonoBehaviour
+{
+
+	public WaypointFollower wf;
+
+	public List<GameObject> waypointGroups;
+	public float minMoveSpeed, maxMoveSpeed;
+
+	void Start()
+	{
+		wf.movementSpeed = Random.Range(minMoveSpeed,maxMoveSpeed);
+		wf.waypoints = new List<Transform>();
+		
+		GameObject waypointParent = (GameObject)Instantiate(Util.GetRandomElement(waypointGroups));
+		waypointParent.transform.rotation *= Util.RandomRotation(Vector3.forward,0,360);
+		waypointParent.transform.position = Vector2.zero;
+		
+		Transform[] points = waypointParent.GetComponentsInChildren<Transform>(true);
+		for(int p = 1; p < points.Length; p++) {
+			wf.waypoints.Add(points[p]);
+		}
+		wf.transform.position = wf.waypoints[0].position;
+	}
+
+}
+
--- Assets/Scripts/ChallengeMode/WorldSpaceScoreAreaLevelConstructor.cs
+++ Assets/Scripts/ChallengeMode/WorldSpaceScoreAreaLevelConstructor.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class StaticScoreAreaLevelConstructor : MonoBehaviour
+public class WorldSpaceScoreAreaLevelConstructor : MonoBehaviour
 {
 
 	public Collider2D area;
