aac4ef074d474a0735aedd113ab849151e016f0c
RaphByrne <raph.byrne@gmail.com>
2015-04-03 16:23:22 +0800

    fixed challenge end screen rank display. Small bug in animator possibly due to unity 5 changes

---
+++ Assets/Scripts/Test/ChallengeEndScreenTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChallengeEndScreenTest : MonoBehaviour
+{
+
+	public AnimatedRankDisplay ard;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ard.SetRank(3);
+	}
+	
+
+}
+
